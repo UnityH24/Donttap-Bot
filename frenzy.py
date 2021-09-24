932e064996729c6cc186eade657862f7c6eec835
UnityH24 <76117104+UnityH24@users.noreply.github.com>
2021-09-24 19:50:17 +0300

    removed some comments

--- frenzy.py
+++ frenzy.py
@@ -5,51 +5,14 @@ import keyboard
 import win32api, win32con
 
 
-# region=(629, 233, 645, 645)
-
-
 def click(x, y):
 	win32api.SetCursorPos((x, y))
-	# moveCursor((x, y))
 	lmb()
 
 def lmb():
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
 	time.sleep(0.005)
 	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-
-def moveCursor(pos, delay=0):
-
-	curr_pos = win32api.GetCursorPos()
-	x = pos[0] - curr_pos[0]
-	y = pos[1] - curr_pos[1]
-
-	x_dir = 1
-	y_dir = 1
-
-	x_neg = str(x).startswith('-')
-	y_neg = str(y).startswith('-')
-
-	if x_neg:
-		x_dir = -1
-
-	if y_neg:
-		y_dir = -1
-
-	# x axis
-	for x_ in range(curr_pos[0], pos[0], x_dir):
-		win32api.SetCursorPos((x_, curr_pos[1]))
-		time.sleep(delay)
-
-
-
-	curr_pos = win32api.GetCursorPos()
-
-	# y axis
-	for y_ in range(curr_pos[1], pos[1], y_dir):
-		win32api.SetCursorPos((curr_pos[0], y_))
-		time.sleep(delay)
-
 
 def main():
 	while not keyboard.is_pressed('q'):
@@ -74,6 +37,8 @@ def main():
 				break
 
 			# time.sleep(0.2)
+			# uncomment the line before if the bot starts clicking on white tiles
+			# you can adjust the delay for your liking
 
 main()
 
